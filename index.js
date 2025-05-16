fd9467e3fd517dbafc011fb103f8a4bc1c4cd62a
DanyRoland <64906201+DanyElAlgo@users.noreply.github.com>
2025-05-16 11:26:28 -0400

    test
    test

--- index.js
+++ index.js
@@ -9,8 +9,16 @@ const html = `<article class="card">
 </article>`;
 
 // 1. Seleccione el container
-
+const container = document.getElementById("container");
 // 2. cree una funciotn createCardComponent.
+function createCardComponent(title, body) {
+  const template = document.createElement("div");
+  template.innerHTML = html;
+  const card = template.firstElementChild;
+  card.querySelector(".card__title").textContent = title;
+  card.querySelector(".card__body__content").textContent = body;
+  return card;
+}
 /**
  * @param {string} title
  * @param {string} body
@@ -20,4 +28,7 @@ const html = `<article class="card">
 
 // 3. Cree un componente
 
+const newCard = createCardComponent("Título 1", "Desc. 1");
+
 // 4. Agrege este nuevo componente al container
+container.appendChild(newCard);
